154ba6eb17e42c5961d33697cc8fa3aa90e4573a
ahmedMahouachi <ahmedmahouachi66@gmail.com>
2026-01-09 01:16:55 +0100

    add file

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ahmedmahouachi66:Aws7rqyif1T3kd4v@clusterapi.az66k47.mongodb.net/");
+        await mongoose.connect("mongodb+srv://ahmedmahouachi66:WIgNZnY8TNNRTLDR@clusterapi.az66k47.mongodb.net/?appName=ClusterAPI");
         console.log("Database connected...");
     } catch (error) {
         console.log("Database connection error:", error);
